58a0f116dc1df4aa0a01759ecddf1c1761e31f20
Juan Francisco Rodriguez Hernandez <juanfrh7@icloud.com>
2026-03-02 18:07:48 -0700

    update class with gpt2 embeddingg

--- src/transformers/word_embedding.py
+++ src/transformers/word_embedding.py
@@ -1,17 +1,19 @@
 import nltk
+import tiktoken
+import torch.nn as nn
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 from gensim.models import Word2Vec
+
 
 nltk.download('punkt_tab')
 nltk.download('stopwords') 
 
 class word_embedding():
-    def __init__(self, text, embedding_dim = 50):
-        self.original_text = text
+    def __init__(self, embedding_type = "gpt2", embedding_dim = 50):
+
+        self.embedding_type = embedding_type
         self.embedding_dim = embedding_dim
-        self.tokens = self.tokenize(text)
-        self.embedding_model = self.create_embedding(self.tokens)
 
     def tokenize(self, text):
         stop_words = set(stopwords.words('english'))
@@ -19,17 +21,34 @@ class word_embedding():
         tokens = [word for word in tokens if word.isalpha() and word not in stop_words]
         return tokens
 
-    def create_embedding(self, tokens):
-        model = Word2Vec(sentences=tokens, 
-                         vector_size=self.embedding_dim, 
-                         min_count=1, seed=42)
-        return model
-    
-    def most_similar_word(self, vector):
-        # vector can be a numpy array or a torch tensor
-        v = vector
-        if hasattr(v, "detach"):  # torch tensor
-            v = v.detach().cpu().numpy()
+    def create_embedding(self, text):
 
-        word, score = self.embedding_model.most_similar(positive=[v], topn=1)[0]
-        return word, score
+        if self.embedding_type == "word2vec":
+            tokens = self.tokenize(text)
+            self.embedding_model = Word2Vec(sentences=tokens, 
+                            vector_size=self.embedding_dim, 
+                            min_count=1, seed=42)
+            embedding = self.embedding_model.wv
+
+            return embedding
+            
+        elif self.embedding_type == "gpt2":
+            encoding_model = tiktoken.get_encoding("gpt2")
+            encoding = encoding_model.encode(text)
+            embedding = nn.Embedding(len(encoding), self.embedding_dim)
+            
+            return encoding, embedding
+    
+    def decode(self, embedding):
+
+        if self.embedding_type == "word2vec":
+            # vector can be a numpy array or a torch tensor
+            v = embedding
+            if hasattr(v, "detach"):  # torch tensor
+                v = v.detach().cpu().numpy()
+
+            word, score = self.embedding_model.most_similar(positive=[v], topn=1)[0]
+            return word, score
+        
+        elif self.embedding_type == "gpt2":
+            return self.embedding_model.decode(embedding)
