5a52189a5bc6237b9e85197fd2dbed4f54b335c2
Juan Francisco Rodriguez Hernandez <juanfrh7@icloud.com>
2026-05-06 12:29:46 -0600

    add entropy eval

---
+++ src/transformers/utils.py
@@ -0,0 +1,15 @@
+import torch
+
+@torch.no_grad()
+def estimate_loss():
+    out = {}
+    model.eval()
+    for split in ['train', 'val']:
+        losses = torch.zeros(eval_iters)
+        for k in range(eval_iters):
+            X, Y = get_batch(split)
+            logits, loss = model(X, Y)
+            losses[k] = loss.item()
+        out[split] = losses.mean()
+    model.train()
+    return out
